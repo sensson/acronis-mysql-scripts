9a5157e43f454fa1a0030574f2a2274a7777e728
ju5t <m@ju5t.nl>
2023-08-26 21:46:59 +0200

    chore: remove old deploy script

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cloudsmith push rpm -k $APIKEY --republish sensson/base/el/7 pkg/rpmbuild/RPMS/x86_64/*.rpm
-cloudsmith push rpm -k $APIKEY --republish sensson/base/el/8 pkg/rpmbuild/RPMS/x86_64/*.rpm

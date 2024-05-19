fcc66ef8433b1318bab5007fea7f0e0c9ebfa79b
ju5t <ju5t@users.noreply.github.com>
2024-05-19 09:20:15 +0200

    chore: add support for ubuntu 24.04

--- deploy-deb.sh
+++ deploy-deb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cloudsmith push deb -k $APIKEY --republish sensson/base/ubuntu/20.04 *.deb
 cloudsmith push deb -k $APIKEY --republish sensson/base/ubuntu/22.04 *.deb
+cloudsmith push deb -k $APIKEY --republish sensson/base/ubuntu/24.04 *.deb
